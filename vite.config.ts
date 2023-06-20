310fd31681dfddc1882e090d4bfc3b18a8ac4920
pascui89 <pascui89@gmail.com>
2023-06-20 13:12:32 +0200

    chore: Update CSS minification configuration
    - Update package-lock.json
    - Update package.json
    - Update vite.config.ts
    - Update postcss.config.cjs
    
    #chore

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteMinifyPlugin({})],
   base: '/inditex-podcast/',
+  build: {
+    minify: 'terser'
+  }
 });
